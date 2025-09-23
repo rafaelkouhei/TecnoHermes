af5f1a4bc74d29c64730631dec508655d521a510
rafaelkouhei <rafaelkouheisumiya@gmail.com>
2025-09-22 21:18:13 -0300

    added layout.html, inventory.html and favicon

--- hermes/urls.py
+++ hermes/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="hermes/index"),
+    path("index", views.index, name="hermes/index"),
     path("login", views.login, name="hermes/login"),
+    path("inventory", views.inventory, name="inventory"),
+    path("add_item", views.add_item, name="add_item")
 ]
--- hermes/views.py
+++ hermes/views.py
@@ -1,9 +1,21 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import datetime
 
 # Create your views here.
 def index(request):
+    now = datetime.datetime.now()
     return render(request, "hermes/index.html")
 
 def login(request):
     return render(request, "hermes/login.html")
+
+# tst products
+item_list = ["first", "second", "third"]
+def inventory(request):
+    return render(request, "hermes/inventory.html", {
+        "item_list": item_list
+    })
+
+def add_item(request):
+    return render(request, "hermes/add_item.html")
